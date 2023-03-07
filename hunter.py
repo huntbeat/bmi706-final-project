8a5d963056e5a28e72e82d25684b3e7534cd69da
Hunter Lee <hyongle@amazon.com>
2023-03-07 15:49:34 -0500

    Refactored into individual files

--- hunter.py
+++ hunter.py
@@ -3,98 +3,6 @@ import pandas as pd
 import streamlit as st
 
 
-@st.cache_data
-def load_mutation_data():
-    df = pd.read_csv("data/data_mutations.txt", sep='\t')
-    return df
-
-
-@st.cache_data
-def load_patient_data():
-    df = pd.read_csv("data/data_clinical_patient.txt", sep='\t')
-    return df
-
-
-@st.cache_data
-def load_sample_data():
-    df = pd.read_csv(
-        "data/data_clinical_sample.txt",
-        sep='\t',
-        skiprows=4)
-    return df
-
-
-@st.cache_data
-def load_cna_data():
-    df = pd.read_csv("data/data_cna.txt", sep='\t')
-    return df
-
-
-sample_df = load_sample_data()
-cna_df = load_cna_data()
-mut_df = load_mutation_data()
-
-all_cancer_types = sample_df["CANCER_TYPE"].unique()
-all_cancer_genes = cna_df['Hugo_Symbol'].unique()
-
-wanted_mutation_types = ['frameshift_variant',
-                         'missense_variant',
-                         'splice_acceptor_variant',
-                         'splice_donor_variant',
-                         'start_lost',
-                         'stop_gained',
-                         'stop_lost']
-all_mutations = list(mut_df['Consequence'].dropna().unique())
-
-valid_mutation_types = []
-for mutation in all_mutations:
-    if not str(mutation):
-        continue
-    for wanted_mutation in wanted_mutation_types:
-        if wanted_mutation in mutation:
-            valid_mutation_types.append(mutation)
-            break
-valid_mutation_types = all_mutations
-
-all_organs = list(filter(lambda column_name: "DMETS_DX_" in column_name, sample_df.columns))
-
-filtered_mut_df = mut_df[mut_df['Consequence'].isin(valid_mutation_types)]
-filtered_cna_df = cna_df
-
-if True:
-    sample_df_filtered_cancer_types = sample_df[sample_df["CANCER_TYPE"].isin(["Breast Cancer"])]
-else:
-    sample_df_filtered_cancer_types = sample_df
-
-# Next, we'll merge copy number alternation data for each of the filtered samples.
-# 1. Transpose the CNA dataframe to turn the sample ID into indices. This will ease the merge.
-cna_genes = set(filtered_cna_df['Hugo_Symbol'].dropna().unique())
-filtered_cna_df_T = filtered_cna_df.set_index('Hugo_Symbol').T
-filtered_cna_df_T.reset_index(inplace=True)
-filtered_cna_df_T = filtered_cna_df_T.add_prefix("cna_")
-filtered_cna_df_T = filtered_cna_df_T.rename(columns={'cna_index': 'SAMPLE_ID'})
-# 2. Merge CNA into sample table.
-sample_cna_df_filtered_cancer_types = sample_df_filtered_cancer_types.merge(filtered_cna_df_T,
-                                                                            on='SAMPLE_ID',
-                                                                            # Left is filtered, right is not
-                                                                            how='left')
-# 3. Wrangle with mutation dataframe.
-mut_genes = set(filtered_mut_df['Hugo_Symbol'].dropna().unique())
-filtered_mut_df = filtered_mut_df[["Hugo_Symbol", "Tumor_Sample_Barcode", "Consequence"]]
-filtered_mut_df = filtered_mut_df.groupby(["Tumor_Sample_Barcode", "Hugo_Symbol"]).size().reset_index(name='Count')
-filtered_mut_df = pd.pivot(filtered_mut_df, columns=['Hugo_Symbol'], index=["Tumor_Sample_Barcode"],
-                           values="Count").add_prefix("mut_").reset_index()
-filtered_mut_df = filtered_mut_df.rename(columns={'Tumor_Sample_Barcode': 'SAMPLE_ID'})
-# 4. Merge mutation into sample table.
-merged_sample_df = sample_cna_df_filtered_cancer_types.merge(filtered_mut_df,
-                                                             on='SAMPLE_ID',
-                                                             # Left is filtered by cancer type, right is not
-                                                             how='left')
-merged_sample_df = merged_sample_df.fillna(0)
-
-st.write(merged_sample_df)
-
-
 def build_heatmap_df(df, genes, organs, max_num_top_genes=20):
     _heatmap_df = []
     top_genes = {}
@@ -133,29 +41,42 @@ def build_heatmap_df(df, genes, organs, max_num_top_genes=20):
 def build_heatmap(heatmap_df):
     # Base chart
     base = alt.Chart(heatmap_df).encode(
-        alt.X('Gene:N'),
-        alt.Y('Organ:N'),
+
     )
 
     # Create Amplification heatmap
-    amp_heatmap = base.mark_rect().encode(
+    amp_heatmap = alt.Chart(heatmap_df).mark_rect().encode(
+        alt.X('Gene:N'),
+        alt.Y('Organ:N'),
         alt.Color('Amplification_Fraction:Q', scale=alt.Scale(scheme='greenblue')),
         opacity=alt.Opacity('Amplification_Fraction:Q', scale=alt.Scale(range=[0.2, 1]), title='fraction'),
     ).properties(title='Amplification Heatmap')
 
     # Create Deletion heatmap
-    del_heatmap = base.mark_rect().encode(
+    del_heatmap = alt.Chart(heatmap_df).mark_rect().encode(
+        alt.X('Gene:N'),
+        alt.Y('Organ:N', axis=alt.Axis(labels=False)),
         alt.Color('Deletion_Fraction:Q', scale=alt.Scale(scheme='greenblue')),
         opacity=alt.Opacity('Deletion_Fraction:Q', scale=alt.Scale(range=[0.2, 1]), title='fraction'),
     ).properties(title='Deletion Heatmap')
 
     # Create Mutation heatmap
-    mut_heatmap = base.mark_rect().encode(
+    mut_heatmap = alt.Chart(heatmap_df).mark_rect().encode(
+        alt.X('Gene:N'),
+        alt.Y('Organ:N', axis=alt.Axis(labels=False)),
         alt.Color('Mutation_Fraction:Q', scale=alt.Scale(scheme='greenblue')),
         opacity=alt.Opacity('Mutation_Fraction:Q', scale=alt.Scale(range=[0.2, 1]), title='fraction'),
     ).properties(title='Mutation Heatmap')
 
-    return amp_heatmap & del_heatmap & mut_heatmap
+    final_heatmap = alt.hconcat(
+        amp_heatmap,
+        del_heatmap,
+        mut_heatmap
+    ).resolve_scale(
+        y='shared'
+    )
+
+    return final_heatmap
 
 def build_chart(heatmap_df, selected_gene):
     filtered_df = heatmap_df[heatmap_df['Gene'] == selected_gene]
@@ -177,11 +98,4 @@ def build_chart(heatmap_df, selected_gene):
         y='Mutation:Q'
     )
     return cna_bar & mut_bar
-
-valid_genes = list(cna_genes.intersection(mut_genes))
-heatmap_df = build_heatmap_df(merged_sample_df, valid_genes, all_organs)
-st.write(heatmap_df)
-st.write(build_heatmap(heatmap_df))
-st.write(build_chart(heatmap_df, 'NCOR1'))
-
 
