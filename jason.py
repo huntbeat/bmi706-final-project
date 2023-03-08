c1c557384f7733373d6028dff824f3be4b3c5dad
Jason Xiang <xiangchuhang@gmail.com>
2023-03-07 22:39:02 -0500

    h1

--- jason.py
+++ jason.py
@@ -4,34 +4,126 @@ import streamlit as st
 #Disable the row limit
 
 @st.cache_data
-def load_data():
-    df = pd.read_table("data/msk_met_2021_clinical_data.tsv", sep='\t')
-    return df
 
-def build_heatmap_types(df, genes, sample_types, max_num_top_genes=20):
-    _heatmap_df = []
-    top_genes = {}
-    for gene in genes:
-        total_cna = df[f'cna_{gene}'].sum()
-        total_mut = df[f'mut_{gene}'].sum()
-        if total_cna == 0 or total_mut == 0:
-            continue
-        top_genes[gene] = total_mut
-        for sample_type in sample_types:
-            gene_data = {'Gene': gene, 'Organ': organ}
-            filtered_df = df[df[sample_type] == 'Yes']
+def count_amplification(df, gene, sample_type):
+    return df[(df[f'cna_{gene}'] > 0) & (df['Sample_Types'] == sample_type)]['SAMPLE_ID'].nunique()
 
-            _amplification = filtered_df[df[f'cna_{gene}'] > 0][f'cna_{gene}'].sum()
-            gene_data['Amplification'] = _amplification
-            gene_data['Amplification_Fraction'] = _amplification / float(total_cna)
-            _deletion = filtered_df[df[f'cna_{gene}'] < 0][f'cna_{gene}'].sum() / float(total_cna)
-            gene_data['Deletion'] = _deletion
-            gene_data['Deletion_Fraction'] = abs(_deletion) / float(total_cna)
-            _mutation = filtered_df[f'mut_{gene}'].sum()
-            gene_data['Mutation'] = _mutation
-            gene_data['Mutation_Fraction'] = _mutation / float(total_mut)
+def count_deletion(df, gene, sample_type):
+    return df[(df[f'cna_{gene}'] < 0) & (df['Sample_Types'] == sample_type)]['SAMPLE_ID'].nunique()
 
-            gene_data['Total MUT'] = total_mut
-            gene_data['Total CNA'] = total_cna
+def count_mutation(df, gene, sample_type):
+    return df[(df[f'mut_{gene}'] != 0) & (df['Sample_Types'] == sample_type)]['SAMPLE_ID'].nunique()
 
-            _heatmap_df.append(gene_data)
+def count_samples(df, sample_type):
+    return df[df['Sample_Types'] == sample_type]['SAMPLE_ID'].nunique()
+
+def fraction(row):
+    total = row['Total']
+    if total == 0:
+        return 0
+    else:
+        return int(row[row.name]) / int(total)
+
+def get_jason_charts(selected_cancer, valid_genes):
+    sample_df = pd.read_csv("data/data_clinical_sample.txt", sep="\t",skiprows=4)
+    cna_df = pd.read_csv("data/data_cna.txt", sep="\t")
+    mut_df = pd.read_csv("data/data_mutations.txt", sep="\t")
+
+    sample_df = pd.read_table("data/msk_met_2021_clinical_data.tsv",sep='\t')
+
+    sample_df['Sample_Types'] = "None"
+    sample_df.loc[(sample_df['Sample Type'] == 'Primary') & (sample_df['Metastatic patient'] == True), 
+                  'Sample_Types'] = "Primary_from_Met"
+    sample_df.loc[(sample_df['Sample Type'] == 'Primary') & (sample_df['Metastatic patient'] == False), 
+                  'Sample_Types'] = "Primary_from_NoMet"
+    sample_df.loc[(sample_df['Sample Type'] == 'Metastasis'), 'Sample_Types'] = "Metastasis"
+
+    sample_df['patient_count'] = 1
+    sample_df = sample_df.rename(columns={'Sample ID': 'SAMPLE_ID'})
+
+    filtered_mut_df = mut_df[mut_df["Hugo_Symbol"].isin(valid_genes)]
+    filtered_cna_df = cna_df[cna_df["Hugo_Symbol"].isin(valid_genes)]
+
+    filtered_cna_df_T = filtered_cna_df.set_index('Hugo_Symbol').T
+    filtered_cna_df_T.reset_index(inplace=True)
+    filtered_cna_df_T = filtered_cna_df_T.add_prefix("cna_")
+    filtered_cna_df_T = filtered_cna_df_T.rename(columns={'cna_index': 'SAMPLE_ID'})
+
+    sample_df_filtered_cancer_types = None
+    if selected_cancer != "Pan Cancer":
+        sample_df_filtered_cancer_types = sample_df[sample_df["Cancer Type"].isin(selected_cancer)]
+    else:
+        sample_df_filtered_cancer_types = sample_df
+
+    filtered_cna_df_T = filtered_cna_df.set_index('Hugo_Symbol').T
+    filtered_cna_df_T.reset_index(inplace=True)
+    filtered_cna_df_T = filtered_cna_df_T.add_prefix("cna_")
+    filtered_cna_df_T = filtered_cna_df_T.rename(columns={'cna_index': 'SAMPLE_ID'})
+    # 2. Merge CNA into sample table.
+    sample_cna_df_filtered_cancer_types = sample_df_filtered_cancer_types.merge(filtered_cna_df_T,
+                                                                                on='SAMPLE_ID',
+                                                                                # Left is filtered, right is not
+                                                                                how='left')
+    # 3. Wrangle with mutation dataframe.
+    filtered_mut_df = filtered_mut_df[["Hugo_Symbol", "Tumor_Sample_Barcode", "Variant_Classification"]]
+    filtered_mut_df = filtered_mut_df.groupby(["Tumor_Sample_Barcode", "Hugo_Symbol"]).size().reset_index(name='Count')
+    filtered_mut_df = pd.pivot(filtered_mut_df, columns=['Hugo_Symbol'], index=["Tumor_Sample_Barcode"], values="Count").add_prefix("mut_").reset_index()
+    filtered_mut_df = filtered_mut_df.rename(columns={'Tumor_Sample_Barcode': 'SAMPLE_ID'})
+    # 4. Merge mutation into sample table.
+    merged_sample_df = sample_cna_df_filtered_cancer_types.merge(filtered_mut_df,
+                                                                on='SAMPLE_ID',
+                                                                # Left is filtered by cancer type, right is not
+                                                                how='left')
+    merged_sample_df = merged_sample_df.fillna(0)
+
+    counts = []
+    Pri_Met = ['Primary_from_Met', 'Primary_from_NoMet', 'Metastasis']
+    primary_from_met_count = count_samples(merged_sample_df, 'Primary_from_Met')
+    primary_from_no_met_count = count_samples(merged_sample_df, 'Primary_from_NoMet')
+    metastasis_count = count_samples(merged_sample_df, 'Metastasis')
+    for gene in valid_genes:
+        for pm in Pri_Met:
+            amplification_count = count_amplification(merged_sample_df, gene,pm)
+            deletion_count = count_deletion(merged_sample_df, gene,pm)
+            mutation_count = count_mutation(merged_sample_df, gene,pm)
+            counts.append({
+                'gene': gene,
+                'Sample_Types': pm,
+                'Amplification': amplification_count,
+                'Deletion': deletion_count,
+                'Mutation': mutation_count,
+                'Total': primary_from_met_count,
+                'Amplification_Fraction': amplification_count / primary_from_met_count,
+                'Deletion_Fraction': deletion_count / primary_from_met_count,
+                'Mutation_Fraction': mutation_count / primary_from_met_count
+            })
+            
+    counts_df = pd.DataFrame(counts)
+
+    # Base chart
+    base = alt.Chart(counts_df).encode(
+        alt.X('gene', sort=alt.EncodingSortField(field='gene', op='count', order='ascending')),
+        alt.Y('Sample_Types', sort=alt.EncodingSortField(field='Sample_Types', op='count', order='ascending')),
+    )
+
+    # Create Amplification heatmap
+    amp_heatmap = base.mark_rect().encode(
+        alt.Color('Amplification_Fraction:Q', scale=alt.Scale(scheme='greenblue')),
+        opacity=alt.Opacity('Amplification_Fraction:Q', scale=alt.Scale(range=[0.2, 1]), title ='fraction'),
+    ).properties(title='Amplification Heatmap')
+
+    # Create Deletion heatmap
+    del_heatmap = base.mark_rect().encode(
+        alt.Color('Deletion_Fraction:Q', scale=alt.Scale(scheme='greenblue')),
+        opacity=alt.Opacity('Deletion_Fraction:Q', scale=alt.Scale(range=[0.2, 1]),title ='fraction'),
+    ).properties(title='Deletion Heatmap')
+
+    # Create Mutation heatmap
+    mut_heatmap = base.mark_rect().encode(
+        alt.Color('Mutation_Fraction:Q', scale=alt.Scale(scheme='greenblue')),
+        opacity=alt.Opacity('Mutation_Fraction:Q', scale=alt.Scale(range=[0.2, 1]),title ='fraction'),
+    ).properties(title='Mutation Heatmap')
+
+    combined_heatmap = amp_heatmap & del_heatmap & mut_heatmap
+    
+    return combined_heatmap
